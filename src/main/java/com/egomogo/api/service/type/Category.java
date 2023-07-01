10092627390acc7d1eef7bccce2f4d83b452a80d
ansm6358 <ansm6358@naver.com>
2023-07-01 17:52:26 +0900

    entity 오타 수정

--- src/main/java/com/egomogo/api/service/type/Category.java
+++ src/main/java/com/egomogo/api/service/type/Category.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Category {
 
-    KOREANFOOD("한식"), WESTERNFOOD("양식"), ASIANFOOD("아시안"),
-    FLOURBASEDFOOD("분식"), MEAT("고기"), CHINESE("중식"),
-    CAFE("카페"), DESSERT("디저트"), CONVENIENCEFOOD("간편식"), JAPANESEFOOD("일식");
+    KOREAN_FOOD("한식"), WESTERN_FOOD("양식"), ASIAN_FOOD("아시안"),
+    FLOUR_BASED_FOOD("분식"), MEAT("고기"), CHINESE_FOOD("중식"),
+    CAFE("카페"), DESSERT("디저트"), CONVENIENCE_FOOD("간편식"), JAPANESE_FOOD("일식");
 
     private final String korean;
 
