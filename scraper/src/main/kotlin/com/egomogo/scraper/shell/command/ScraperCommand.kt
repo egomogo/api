65ec7763ca9038884a78ee5a8cf97631a29941a1
hseungho <hsh47607@naver.com>
2023-07-11 00:32:30 +0900

    Feat: scraper command called scraper service logic

--- scraper/src/main/kotlin/com/egomogo/scraper/shell/command/ScraperCommand.kt
+++ scraper/src/main/kotlin/com/egomogo/scraper/shell/command/ScraperCommand.kt
@@ -1,14 +1,18 @@
 package com.egomogo.scraper.shell.command
 
+import com.egomogo.scraper.shell.appservice.ScarperService
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 
 @ShellComponent
-class ScraperCommand {
+class ScraperCommand(
+        private val scraperService: ScarperService
+) {
 
     @ShellMethod(key = ["scrap"], value = "start scarping menu of restaurants doesn't have menus")
-    fun startScrap() {
-
+    fun startScrap() : String {
+        val scrapedCount = scraperService.saveScarpedMenuResult()
+        return "Success Scraped Result Count: $scrapedCount"
     }
 
 }
