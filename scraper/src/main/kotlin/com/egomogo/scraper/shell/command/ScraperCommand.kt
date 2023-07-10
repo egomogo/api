6b6f7e34bc8fae66aea9908e3860b84beb47ccdb
hseungho <hsh47607@naver.com>
2023-07-10 17:30:25 +0900

    Chore: created shell command class

---
+++ scraper/src/main/kotlin/com/egomogo/scraper/shell/command/ScraperCommand.kt
@@ -0,0 +1,14 @@
+package com.egomogo.scraper.shell.command
+
+import org.springframework.shell.standard.ShellComponent
+import org.springframework.shell.standard.ShellMethod
+
+@ShellComponent
+class ScraperCommand {
+
+    @ShellMethod(key = ["scrap"], value = "start scarping menu of restaurants doesn't have menus")
+    fun startScrap() {
+
+    }
+
+}
