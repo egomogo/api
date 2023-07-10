555d5f14ed1300054b257c545c451e8f3c4a543b
hseungho <hsh47607@naver.com>
2023-07-10 17:14:47 +0900

    Feat: created Scraper interface

---
+++ scraper/src/main/kotlin/com/egomogo/scraper/scrap/Scraper.kt
@@ -0,0 +1,13 @@
+package com.egomogo.scraper.scrap
+
+interface Scraper<T> {
+    fun scrap(data: List<T>) : List<T>
+
+    fun sleep(millis: Long) {
+        try {
+            Thread.sleep(millis)
+        } catch (e : InterruptedException) {
+            throw RuntimeException(e)
+        }
+    }
+}
