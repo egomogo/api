dd49913e1cba36460b74396cf80a2a70de4fa55d
hseungho <hsh47607@naver.com>
2023-07-11 12:21:51 +0900

    Feat: defined query for select random restaurant with categories

--- domain/src/main/java/com/egomogo/domain/repository/RestaurantRepository.java
+++ domain/src/main/java/com/egomogo/domain/repository/RestaurantRepository.java
@@ -2,11 +2,14 @@ package com.egomogo.domain.repository;
 
 import com.egomogo.domain.dto.IRestaurantDistanceDto;
 import com.egomogo.domain.entity.Restaurant;
+import com.egomogo.domain.type.CategoryType;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, String> {
@@ -21,4 +24,15 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, String> 
     )
     Slice<IRestaurantDistanceDto> findByRandomAndDistance(Long seed, Double userX, Double userY, Integer distanceLimit, Pageable pageable);
 
+    @Query(
+            value = "SELECT *, " +
+                    "ST_Distance_Sphere(POINT(:userX, :userY), POINT(r.x, r.y)) as distance " +
+                    "FROM restaurant as r, restaurant_categories as rc " +
+                    "WHERE rc.categories IN (:categories) " +
+                    "HAVING distance <= : distanceLimit " +
+                    "ORDER BY RAND(:seed)",
+            nativeQuery = true
+    )
+    Slice<IRestaurantDistanceDto> findByRandomAndDistanceAndCategories(Long seed, Double userX, Double userY, Integer distanceLimit,
+                                                                       Collection<CategoryType> categories, Pageable pageable);
 }
