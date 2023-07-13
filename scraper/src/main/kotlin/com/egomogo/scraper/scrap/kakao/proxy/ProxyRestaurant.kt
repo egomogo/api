59dbb17f9edab31137454b3c4fc86ed4d316989a
hseungho <hsh47607@naver.com>
2023-07-13 16:45:15 +0900

    Add test for save scraping result to db that is in @ScraperServiceImplUnitTest

--- scraper/src/main/kotlin/com/egomogo/scraper/scrap/kakao/proxy/ProxyRestaurant.kt
+++ scraper/src/main/kotlin/com/egomogo/scraper/scrap/kakao/proxy/ProxyRestaurant.kt
@@ -6,9 +6,9 @@ data class ProxyRestaurant(
         val proxyId: String,
         val proxyName: String,
         val proxyKakaoPlaceId: String,
+        var proxyScrapedAt: LocalDateTime? = null,
         var proxyMenus: ArrayList<ProxyMenu> = ArrayList()
 ) {
-    lateinit var proxyScrapedAt: LocalDateTime
 
     fun addProxyMenu(proxyMenu: ProxyMenu) {
         this.proxyMenus.add(proxyMenu)
