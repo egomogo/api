23dee24e92270c204752758532739a1484be1299
hseungho <hsh47607@naver.com>
2023-07-13 15:31:22 +0900

    Add scrapedAt field to @ProxyRestaurant

--- scraper/src/main/kotlin/com/egomogo/scraper/scrap/kakao/proxy/ProxyRestaurant.kt
+++ scraper/src/main/kotlin/com/egomogo/scraper/scrap/kakao/proxy/ProxyRestaurant.kt
@@ -1,9 +1,12 @@
 package com.egomogo.scraper.scrap.kakao.proxy
 
+import java.time.LocalDateTime
+
 data class ProxyRestaurant(
         val proxyId: String,
         val proxyName: String,
         val kakaoPlaceId: String,
+        var scrapedAt: LocalDateTime,
         var menus: ArrayList<ProxyMenu> = ArrayList()
 ) {
     fun addMenu(menu: ProxyMenu) {
